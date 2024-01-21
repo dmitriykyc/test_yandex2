e8a71e503af39f5cec2ceef1885cabd5c43d3903
KusokMAC <dmitriykyc@gmail.com>
2024-01-21 19:44:49 +0300

    download project

---
+++ main.py
@@ -0,0 +1,10 @@
+from config import TOKEN
+from telebot import TeleBot
+
+bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def bot_start(message):
+    print('Получена команда /start')
+    bot.send_message(message.chat.id, text="Я готов к работе!")
